34cbc2fa1b4708e02c65d96e42c77444f668de56
drewlesueur <drewalex@gmail.com>
2012-05-29 05:06:04 +0000

    parsing

--- thumbs.js
+++ thumbs.js
@@ -600,6 +600,57 @@ var runFile = function (file) {
   return ran;
 } 
 
+var parse = function (code) {
+  var oneLineStrings = function (code) {
+    var newCode = [] 
+    var len = code.length
+    var chr;
+    var mode = "new-line";
+    var indentCount = 0;
+    var isSpace = function () { return chr == " "}
+    var isNewLine = function () { return chr == "\n"}
+    var isStringChr = function () { return chr != " " && chr != "\n" && chr != "\r" && chr != "\t"}
+    var isStartString = funciton () { return chr == symbols.string || symbols["string-literal"] }
+    var finalBucket = []
+    var callBucket = []
+    var wordBucket = []
+
+    var handleNewLine = function () {
+      if (isNewLine()) {
+        indentCount == 0 
+      } else if (isSpace()) {
+        indentCount += 1 
+      } else if (isStringChr()) {
+         
+      }
+    }
+
+    var handleCode = function () {
+      if (isStartString()) {
+        mode = "string" 
+      }
+    }
+
+    var handleString = function () {
+    
+    }
+
+    for (var i; i < len; i++) {
+      chr = code.charAt(i);
+
+      if (mode == "new-line") {
+        handleNewLine()
+      } else if (mode = "code") {
+        handleCode()
+      } else if (mode == "string") {
+        handleString() 
+      }
+    }
+  }
+  code = oneLineStrings(code)
+}
+
+thumbs.parse = parse
 thumbs.runScripts = runScripts
 thumbs.run = run //runs raw code
 thumbs.runFile = runFile
