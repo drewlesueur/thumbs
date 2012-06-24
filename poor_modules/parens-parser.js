3480220f15d34112d2f81d95ee3c5868be0bcb06
drewlesueur <drewalex@gmail.com>
2012-06-24 17:33:10 +0000

    better strings

--- poor_modules/parens-parser.js
+++ poor_modules/parens-parser.js
@@ -8,20 +8,33 @@ setModule("parens-parser", function () { return function (code) {
       var addWord = function () { if (word.length) group.push(word); }
       var resetWord = function () { word = "" }
       var handleSpace = function () { addWord(); resetWord(); }
-      var handleQuote = function () { }
+      var handleQuote = function () { strName = word; resetWord(); state = "text"; }
       var handleWord = function () { word += chr }
-      var handleCode = function () {
+      var handleEscapeChar = function () { state = "escaped-text" }
+      var handleEndQuote = function () { 
+        if (strNameIsNext()) { 
+          state = "code"; i += strName.length;
+          group.push(word); resetWord(); return; }
+        word += chr;
+      }, strNameIsNext = function () {
+        var next = code.substr(i+1, strName.length);
+        if (next == strName) return true;
+        return false;
+      }, handleCode = function () {
         if (isStartParens())  return handleStartParens();
         if (isEndParens()) return handleEndParens()
         if (isSpaceLike()) return handleSpace()
         if (isQuote()) return handleQuote()
         handleWord()
-      }
-      var isStartParens = function () { return chr == "("; }
+      }, handleText = function () {
+        if (isQuote()) return handleEndQuote();
+        handleWord();
+      }, isStartParens = function () { return chr == "("; }
       var isEndParens = function () { return chr == ")"; }
       var isSpaceLike = function () { return chr == " " || chr == "\n" || chr == "\r" || chr == "\t" }
       var isQuote = function () { return chr == "\"" }
-      var word = "", state = "code", group = [], chr = "";
+      var isEscapeChar = function () { return chr == "\\" }
+      var word = "", state = "code", group = [], chr = "", strName = "";
       while (i < codeLength) {
         chr = code.charAt(i);
         if (state == "code") { ret = handleCode() } 
